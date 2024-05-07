3ad0d11e4acdf8050397294eacf9a839b871fc6c
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-05-07 11:59:25 +0100

    Template Literal

--- script.js
+++ script.js
@@ -174,3 +174,9 @@ const updatedBook = {
   pages: 1210,
 };
 updatedBook;
+
+//Template literals
+const summary = `New book here - ${title} and it is a ${pages}-page long book, was written by ${author} and published in ${
+  publicationDate.split("-")[0]
+}`;
+summary;
