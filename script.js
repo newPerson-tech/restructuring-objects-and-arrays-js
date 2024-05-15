16de88d5049d603562aabbe8390ec9c13ae2659a
Tetiana Paniot <tatyana.paniot@gmail.com>
2024-05-15 10:07:01 +0100

    Async/Await function

--- script.js
+++ script.js
@@ -292,8 +292,18 @@ booksAfterUpdate;
 
 //Promises
 
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log("tetiana");
+// console.log("tetiana");
+
+//Acync / Await
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+getTodos();
